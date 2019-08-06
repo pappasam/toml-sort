a2ec3582e7f7de2d889b3a1d5f48157a8fbbf0e4
Sam Roeca <samuel.roeca@gmail.com>
2019-08-06 09:52:32 -0400

    Add output CLI option, standardize CLI, add docs

--- toml_sort/cli.py
+++ toml_sort/cli.py
@@ -6,9 +6,34 @@ from . import sort_toml
 
 
 @click.command()
-@click.argument("filename")
-def cli(filename):
-    """Simple command line interface, work in progress"""
-    with open(filename, "r") as infile:
-        toml_content = infile.read()
-    click.echo(sort_toml(toml_content))
+@click.option(
+    "-o",
+    "--output",
+    type=click.File("w"),
+    default="-",
+    show_default=True,
+    help="The output filepath. Choose stdout with '-'.",
+)
+@click.argument("filename", type=click.File("r"))
+def cli(output, filename) -> None:
+    """Sort toml file FILENAME, saving results to a file, or stdout (default)
+
+    FILENAME a filepath or standard input (-)
+
+    Examples:
+
+        Read from stdin, write to stdout:
+
+            cat input.toml | toml-sort -
+
+        Read from file on disk, write to file on disk:
+
+            toml-sort -o output.toml input.toml
+
+        Read from file on disk, write to stdout
+
+            toml-sort input.toml
+    """
+    toml_content = filename.read()
+    sorted_toml = sort_toml(toml_content)
+    output.write(sorted_toml)
