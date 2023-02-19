95e6c430e36c29a324619f4fbdaf9003d00f7456
Jonathan Green <jonathan@razzard.com>
2023-02-18 22:47:15 -0400

    Replace with old style union for compatibility.

--- toml_sort/settings.py
+++ toml_sort/settings.py
@@ -1,5 +1,5 @@
 """TomlSort pyproject settings validation model."""
-from __future__ import annotations
+from typing import Optional
 
 from pydantic import BaseModel, Extra, PositiveInt
 
@@ -7,22 +7,22 @@ from pydantic import BaseModel, Extra, PositiveInt
 class Settings(BaseModel):
     """Used to validate settings loaded from pyproject.toml."""
 
-    all: None | bool
-    in_place: None | bool
-    no_header: None | bool
-    no_comments: None | bool
-    no_header_comments: None | bool
-    no_footer_comments: None | bool
-    no_inline_comments: None | bool
-    check: None | bool
-    ignore_case: None | bool
-    no_sort_tables: None | bool
-    sort_inline_tables: None | bool
-    sort_inline_arrays: None | bool
-    sort_table_keys: None | bool
-    spaces_before_inline_comment: None | PositiveInt
-    spaces_indent_inline_array: None | PositiveInt
-    trailing_comma_inline_array: None | bool
+    all: Optional[bool]
+    in_place: Optional[bool]
+    no_header: Optional[bool]
+    no_comments: Optional[bool]
+    no_header_comments: Optional[bool]
+    no_footer_comments: Optional[bool]
+    no_inline_comments: Optional[bool]
+    check: Optional[bool]
+    ignore_case: Optional[bool]
+    no_sort_tables: Optional[bool]
+    sort_inline_tables: Optional[bool]
+    sort_inline_arrays: Optional[bool]
+    sort_table_keys: Optional[bool]
+    spaces_before_inline_comment: Optional[PositiveInt]
+    spaces_indent_inline_array: Optional[PositiveInt]
+    trailing_comma_inline_array: Optional[bool]
 
     class Config:  # pylint: disable=too-few-public-methods
         """Pydantic model configuration."""
